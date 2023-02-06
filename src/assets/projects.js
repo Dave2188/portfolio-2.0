668d9327b2a8fe024702a3b74ad88b5e3ae2605f
David fox <davidfox967@yahoo.com>
2023-02-05 23:36:59 -0700

    photos to public

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,48 +1,48 @@
 const projectData = [
 	{
-		img: "src/assets/jobsiteTracker.jpg",
+		img: "/jobsiteTracker.jpg",
 		title: "Job Site Tracker",
 		description:
 			"This React app was built for tracking job sites in the oil field. The user can add, edit, move, delete, and print job material lists. The forms also allow the addition of dynamic sections as needed along with using gps location. When a job list is printed, it takes the gps location and generates a QR code so others can receive directions with ease. This project was a valuable experience in the back-end development of applications and deploying them across multiple hosting services.",
 		links: ["https://jobsite-tracker.netlify.app", "https://github.com/Dave2188/Job-Site-Tracker"],
 	},
 	{
-		img: "src/assets/portfoliov2.jpg",
+		img: "/portfoliov2.jpg",
 		title: "Portfolio v-2",
 		description:
 			"The portfolio page you're browsing now was built using Vite/Vue and Tailwind. After learning React, I wanted to broaden my skill set a little further. I saw this as a perfect opportunity to challenge myself with what I've learned by updating my portfolio page. The Composition API certainly made it easier to keep related data together. However, in my opinion it loses the pre-defined structure of the Options API. I do agree with the overwhelming consensus;  Vue has less of a learning curve when compared to React, though it is not without its quirks. ",
 		links: ["https://www.dfoxden.com/", "https://github.com/Dave2188/portfolio-2.0"],
 	},
 	{
-		img: "src/assets/portfoliov1.jpg",
+		img: "/portfolioV1.jpg",
 		title: "Portfolio v-1",
 		description:
 			"This was the first portfolio page I built to showcase my accomplishments through projects I've completed. Looking back, I'm proud of how it turned out given my skills at the time. It was a welcome challenge to design a layout of my own and troubleshoot issues as they surfaced.",
 		links: ["https://dave2188.github.io/Portfolio-page/", "https://github.com/Dave2188/Portfolio-page"],
 	},
 	{
-		img: "src/assets/vueReaction.jpg",
+		img: "/vueReaction.jpg",
 		title: "Vue Reaction",
 		description:
 			"I started my journey learning Vue with a course from The Net Ninja. Building this small game helped me learn the basics of Vue 3. In the course, the modal that pops up is always in the same place. To make things more challenging, I added some code to make the modal appear in random places.",
 		links: ["https://teal-bonbon-04d771.netlify.app/", "https://github.com/Dave2188/reaction-time-app"],
 	},
 	{
-		img: "src/assets/playlistify.jpg",
+		img: "/playlistify.jpg",
 		title: "Playlistify",
 		description:
 			"This class-based React app allows the user to create playlists on Spotify. Search by song title, artist, or album. Add songs to the playlist title of your choosing which will then be saved to your spotify account upon submission. At the time, this project was my first encounter with third party authorizations.",
 		links: ["http://play-list-ify.surge.sh/", "https://github.com/Dave2188/jamming"],
 	},
 	{
-		img: "src/assets/battleship.jpg",
+		img: "/battleship.jpg",
 		title: "Battleship",
 		description:
 			"I built this game with vanilla javascript to mimic the classic board game Battleship. A CSS grid is over layed atop a background image of the game board. The grid allows you to set ship locations so that hits and misses can be reflected on the image.",
 		links: ["https://dave2188.github.io/mybattleship/", "https://github.com/Dave2188/mybattleship"],
 	},
 	{
-		img: "src/assets/moshify.jpg",
+		img: "/moshify.jpg",
 		title: "Moshify",
 		description:
 			"This is a mock site that I put together while learning the basics of web development in a course from Coding with Mosh. Even though it was part of a course, it was far from seamless. Due to the nature of technology, courses and information tend to become outdated or just simply not work as intended. I took it upon myself to work through these issues as they presented themselves and learned a ton in the process.",
